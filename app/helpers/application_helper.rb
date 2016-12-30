cdabb8a6c2a6d04e613b78bbfcce31c4b078d9ec
yo suzuki <suzuki_yoshiharu@3-ize.jp>
2016-12-30 14:21:36 +0900

    db.yml修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,23 @@ module ApplicationHelper
 	
 def profile_img(user)
   #binding.pry
-	#return image_tag(user.avatar_url) if user.avatar_url?
+  #img_url = user.avatar_url
+	#return image_tag(user.avatar_url) if user.avatar_url ?
 	# return image_tag(user.img_url) if user.img_url?
+
      unless user.provider.blank?
        img_url = user.avatar_url
+       #image_tag(user.avatar_url)
     else
-      img_url = 'no_image.png'
-   end
+      img_url = "https://riracle.com/images/common/noimage.png"
+      #image_tag(img_url)
+    end
+    if img_url..nil?
+      image_tag(img_url)
+    else
+      image_tag('no_image.png')
+    end
 #binding.pry
-   image_tag(img_url)
+   
   end
 end
