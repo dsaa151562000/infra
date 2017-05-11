382b53b56be1157b80c79ead22ffa48e8c0af7e1
suzuki <suzuki02yo@gmail.com>
2017-05-11 21:51:12 +0900

    test

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Infra
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.log_level = :error
+
     config.i18n.default_locale = :ja
     config.generators do |g|
       g.test_framework :rspec,
