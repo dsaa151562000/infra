57f0bf8c02a13537f6427f5bb30d9dafc82589d0
suzuki <suzuki02yo@gmail.com>
2017-05-17 20:54:04 +0900

    test

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -20,7 +20,7 @@ class RestaurantsController < ApplicationController
 
     session[:query] = query
     url = build_url(query)
-    url = URI.escape(url)
+    #url = URI.escape(url)
     res = open(url)
 
     if res.status[0] == "200"
@@ -35,7 +35,7 @@ class RestaurantsController < ApplicationController
   def detail
     id = params[:id]
     url = build_url({ id: id })
-    URI.parse URI.encode
+    #URI.parse URI.encode
     res = open(url)
 
     if res.status[0] == "200"
@@ -50,28 +50,15 @@ class RestaurantsController < ApplicationController
     def build_url(query)
       url = 'http://api.gnavi.co.jp/RestSearchAPI/20150630/'
 
-
       query[:keyid] = "8702b77b4df3f0b9ee6c1dc30695c381"
       query[:format] = 'json'
 
-
-      url = url + '?' + 'keyid=' + query[:keyid] + '&format='+ query[:format]+'&name='+ query[:name]
+      url = url + '?' + query.to_query
+      #url = url + '?' + 'keyid=' + query[:keyid] + '&format='+ query[:format]+'&name='+ query[:name]
      #binding.pry
 
     end
-
-  def build_url02(query)
-    url = 'http://api.gnavi.co.jp/RestSearchAPI/20150630/'
-
-
-    query[:keyid] = "8702b77b4df3f0b9ee6c1dc30695c381"
-    query[:format] = 'json'
-
-
-    url = url + '?' + 'keyid=' + query[:keyid] + '&format='+ query[:format]+'&name='+ query[:name]+'&id='+ query[:id]
-    #binding.pry
-
-  end
+  
 end
 
 
