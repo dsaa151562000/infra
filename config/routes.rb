fa9c5a8dc2240e4bebee9ab8fb4321c532cde404
suzuki <suzuki02yo@gmail.com>
2017-05-20 10:52:12 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   #root :to => "blogs#index"
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'restrants',   to: 'restaurants#top'
-  #root :to => "restaurants#top"
+  #get 'restrants',   to: 'restaurants#top'
+  root :to => "restaurants#top"
 
   post 'list',      to: 'restaurants#list'
   get 'detail/:id', to: 'restaurants#detail', as: 'detail'
